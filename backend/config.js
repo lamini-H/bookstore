b866d031489e94d39f7a97fd9f693d579a057281
lamini-H <laminiharunah@gmail.com>
2024-07-23 15:27:08 +0200

    removed the connection string

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 
-export const mongoDBURL = 'mongodb+srv://root:ucVjCakr0V3AW0fg@cluster0.wefc6yi.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
+export const mongoDBURL = 'add your connection string here'
